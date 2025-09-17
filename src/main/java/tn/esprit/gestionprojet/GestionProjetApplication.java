5963b480356c1d95ee5f6f5d15d823ea423479a6
jebali.hassen <hdjebali92@gmail.com>
2025-09-17 11:16:12 +0100

    first commit

---
+++ src/main/java/tn/esprit/gestionprojet/GestionProjetApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestionprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionProjetApplication.class, args);
+    }
+
+}
